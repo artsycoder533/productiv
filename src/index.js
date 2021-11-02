d0e978e1933e6d82f815991733b633f0a1f9692f
Natasha <njohnson533@gmail.com>
2021-11-02 00:08:52 -0400

    added createElement.js module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/style.scss";
 import { getElement } from "./scripts/getElement.js";
+import { createElement, createElementWithClass, createTextElement, createLink } from "./scripts/createElement.js";
 
 //
 
---
+++ src/scripts/createElement.js
@@ -0,0 +1,27 @@
+function createElement(type) {
+    const el = document.createElement(type);
+    return el;
+}
+
+function createElementWithClass(type, className) {
+    const el = document.createElement(type);
+    document.classList.add(className);
+    return el;
+}
+
+function createTextElement(type, className, text) {
+    const el = document.createElement(type);
+    el.classList.add(className);
+    el.textContent = text;
+    return el;
+}
+
+function createLink(type, className, src, text) {
+    const el = document.createElement(type);
+    el.classList.add(className);
+    el.src = src;
+    el.textContent = text;
+    return el;
+}
+
+export { createElement, createElementWithClass, createTextElement, createLink };
