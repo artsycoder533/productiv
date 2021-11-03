a7bec878d24ca97b29b1d142dd4229458e6e8d48
Natasha <njohnson533@gmail.com>
2021-11-03 17:48:16 -0400

    added sidebar.js, imported to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import "./styles/style.scss";
-// import { getElement } from "./scripts/getElement.js";
-// import { renderHeader } from "./scripts/header.js";
-// import { renderSidebar } from "./scripts/sidebar.js";
+import { addSidebarEvents } from "./scripts/sidebar.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    // document.body.appendChild(renderHeader());
-    // document.body.appendChild(renderSidebar());
+    addSidebarEvents();
 });
     
 
---
+++ src/scripts/sidebar.js
@@ -0,0 +1,45 @@
+const sidebarDivs = [...document.querySelectorAll(".sidebar__link")];
+
+
+function addSidebarEvents() {
+    sidebarDivs.map((div) => {
+    div.addEventListener("click", (e) => {
+        openSubMenu(e);
+    });
+});
+}
+
+
+
+function openSubMenu(e) {
+    const display = document.querySelector(".header__display");
+	// console.log(e.currentTarget.dataset.id);
+	display.textContent = e.currentTarget.dataset.id;
+	removeActiveLinks(e);
+	closeSubMenus();
+	if (e.currentTarget.nextElementSibling) {
+		e.currentTarget.classList.toggle("active");
+		e.currentTarget.nextElementSibling.classList.toggle("showSub");
+	} else {
+		e.currentTarget.classList.add("active");
+	}
+}
+
+function removeActiveLinks() {
+	sidebarDivs.map((div) => {
+		div.classList.remove("active");
+	});
+}
+
+function closeSubMenus() {
+	const submenus = [...document.querySelectorAll(".sidebar__sublinks")];
+	submenus.map((menu) => {
+		if (menu.classList.contains("showSub")) {
+			return;
+		} else {
+			menu.classList.remove("showSub");
+		}
+	});
+}
+
+export { addSidebarEvents };
