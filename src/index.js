3614ddb57a315d322a1609c15ece31bb2cd32dfc
Natasha <njohnson533@gmail.com>
2021-11-03 13:57:20 -0400

    added event listener to window and moved call to renderheader inside

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import "./styles/style.scss";
 import { getElement } from "./scripts/getElement.js";
 import { renderHeader } from "./scripts/header.js";
 
-
+window.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(renderHeader());
+});
+    
 
 
 //render header
