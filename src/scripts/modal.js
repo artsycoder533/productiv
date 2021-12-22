f264e0c745e3b47b653aab5b7263a7d3f9d25054
Natasha <njohnson533@gmail.com>
2021-12-21 19:05:45 -0500

    added form validation

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -13,8 +13,44 @@ function addButtonEvents() {
     cancelBtn.addEventListener("click", toggleModal);
     closeBtn.addEventListener("click", toggleModal);
     createBtn.addEventListener("click", (e)=>{
-        populateTodo(e);
+        validateForm(e);
     });
+}
+
+function validateForm(e) {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const date = document.getElementById("date").value;
+    const priority = document.getElementById("priority").value;
+    const project = document.getElementById("project").value;
+    const titleError = document.querySelector(".title__error");
+    const messageError = document.querySelector(".message__error");
+    const dateError = document.querySelector(".date__error");
+    const currentDate = new Date();
+    console.log(currentDate);
+    if (title.trim().match(/\s*/)) {
+        titleError.textContent = "Title must not be blank";
+        setTimeout(function () {
+            titleError.textContent = " ";
+        }, 3000);
+        
+    }
+    if (description.trim().match(/\s*/)) {
+        messageError.textContent = "Message must not be blank";
+        setTimeout(function () {
+            messageError.textContent = " ";
+        }, 3000);
+    }
+    if (date === "") {
+        dateError.textContent = "Please select a date";
+        setTimeout(function () {
+            dateError.textContent = " ";
+        }, 3000);
+    }
+    else {
+        populateTodo(e);
+    }
 }
 
 function toggleModal() {
