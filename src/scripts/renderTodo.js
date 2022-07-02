e6aca32bef7d77a2d3cd0dedbcd0548c1b8b4e16
Natasha <njohnson533@gmail.com>
2022-07-01 21:36:00 -0400

    added overdue warning to tasklist

--- src/scripts/renderTodo.js
+++ src/scripts/renderTodo.js
@@ -1,78 +1,94 @@
 import { createElementWithClass, createTextElementWithClass, createElementWithAttribute } from "./createElement.js";
-import { toggleModal } from "./modal.js";
+import { getTodaysDate, toggleModal } from "./modal.js";
 import { getTodoById, updateTodo, deleteTodo, getADocument} from "./todo.js";
 
 const todos = createElementWithClass("section", "todos");
 
 function renderTodo(title, description, date, priority, project, id) {
-    const todo__container = createElementWithClass("div", "todo__container");
-    todo__container.setAttribute("data-id", id);
-    const todo = createElementWithClass("article", "todo");
-    const todo__date = createElementWithClass("div", "todo__date");
-    const date1 = createTextElementWithClass("small", "date", date);
-    todo__date.appendChild(date1);
-    const todo__project = createTextElementWithClass(
-      "div",
-      "todo__project",
-      project
-    );
-    todo__date.appendChild(todo__project);
-    todo.appendChild(todo__date);
-    
-
-    const todo__content = createElementWithClass("div", "todo__content");
-    const todo__title = createTextElementWithClass("div", "todo__title", title);
-    const todo__priority = createElementWithClass("div", "todo__priority");
-
-    const priority1 = createTextElementWithClass("small", "priority", priority);
-    if (priority === "high") {
-        priority1.classList.add("high");
+  const todo__container = createElementWithClass("div", "todo__container");
+  todo__container.setAttribute("data-id", id);
+  const todo = createElementWithClass("article", "todo");
+  const todo__date = createElementWithClass("div", "todo__date");
+  const date1 = createTextElementWithClass("small", "date", date);
+  //check for overdue dates
+    if (date1.textContent <= getTodaysDate()) {
+        date1.classList.add("overdue");
+        date1.textContent += ' OVERDUE !!';
     }
-    else {
-        priority1.classList.add("normal");
-    }
-    todo__priority.appendChild(priority1);
-    todo__content.appendChild(todo__title);
-    todo__content.appendChild(todo__priority);
+  todo__date.appendChild(date1);
+  const todo__project = createTextElementWithClass(
+    "div",
+    "todo__project",
+    project
+  );
+  todo__date.appendChild(todo__project);
+  todo.appendChild(todo__date);
 
-    //todo buttons
-    const todo__buttons = createElementWithClass("div", "todo__buttons");
-    const todo__more = createElementWithAttribute("button", "todo__more", "id", "more");
-    const moreBtn = createElementWithClass("i", "fas");
-    moreBtn.classList.add("fa-caret-down");
-    moreBtn.addEventListener("click", showDetails);
-    todo__more.appendChild(moreBtn);
-    todo__buttons.appendChild(todo__more);
-    const todo__edit = createElementWithAttribute("button", "todo__edit", "id", "edit");
-    const editBtn = createElementWithClass("i", "fas");
-    editBtn.classList.add("fa-edit");
-    editBtn.addEventListener("click", editTodo);
-    todo__edit.appendChild(editBtn);
-    todo__buttons.appendChild(todo__edit);
-    const todo__delete = createElementWithAttribute("button", "todo__delete", "id", "delete");
-    const deleteBtn = createElementWithClass("i", "fas");
-    deleteBtn.classList.add("fa-trash-alt");
-    deleteBtn.addEventListener("click", deleteTodoUI);
-    todo__delete.appendChild(deleteBtn);
-    todo__buttons.appendChild(todo__delete);
-    todo__content.append(todo__buttons);
-    todo.appendChild(todo__content);
+  const todo__content = createElementWithClass("div", "todo__content");
+  const todo__title = createTextElementWithClass("div", "todo__title", title);
+  const todo__priority = createElementWithClass("div", "todo__priority");
 
-    
-    //todo.appendChild(todo__project);
-    todo__container.appendChild(todo);
-    
+  const priority1 = createTextElementWithClass("small", "priority", priority);
+  if (priority === "high") {
+    priority1.classList.add("high");
+  } else {
+    priority1.classList.add("normal");
+  }
+  todo__priority.appendChild(priority1);
+  todo__content.appendChild(todo__title);
+  todo__content.appendChild(todo__priority);
 
-    // description
-    const todo__details = createElementWithClass("div", "todo__details");
-    const todo__description = createElementWithClass("p", "description");
-    todo__description.textContent = description;
-    const todo__fulltitle = createElementWithClass("p", "fulltitle");
-    todo__fulltitle.textContent = title;
-    todo__details.appendChild(todo__fulltitle);
-    todo__details.appendChild(todo__description);
-    todo__container.appendChild(todo__details);
-    return todo__container;
+  //todo buttons
+  const todo__buttons = createElementWithClass("div", "todo__buttons");
+  const todo__more = createElementWithAttribute(
+    "button",
+    "todo__more",
+    "id",
+    "more"
+  );
+  const moreBtn = createElementWithClass("i", "fas");
+  moreBtn.classList.add("fa-caret-down");
+  moreBtn.addEventListener("click", showDetails);
+  todo__more.appendChild(moreBtn);
+  todo__buttons.appendChild(todo__more);
+  const todo__edit = createElementWithAttribute(
+    "button",
+    "todo__edit",
+    "id",
+    "edit"
+  );
+  const editBtn = createElementWithClass("i", "fas");
+  editBtn.classList.add("fa-edit");
+  editBtn.addEventListener("click", editTodo);
+  todo__edit.appendChild(editBtn);
+  todo__buttons.appendChild(todo__edit);
+  const todo__delete = createElementWithAttribute(
+    "button",
+    "todo__delete",
+    "id",
+    "delete"
+  );
+  const deleteBtn = createElementWithClass("i", "fas");
+  deleteBtn.classList.add("fa-trash-alt");
+  deleteBtn.addEventListener("click", deleteTodoUI);
+  todo__delete.appendChild(deleteBtn);
+  todo__buttons.appendChild(todo__delete);
+  todo__content.append(todo__buttons);
+  todo.appendChild(todo__content);
+
+  //todo.appendChild(todo__project);
+  todo__container.appendChild(todo);
+
+  // description
+  const todo__details = createElementWithClass("div", "todo__details");
+  const todo__description = createElementWithClass("p", "description");
+  todo__description.textContent = description;
+  const todo__fulltitle = createElementWithClass("p", "fulltitle");
+  todo__fulltitle.textContent = title;
+  todo__details.appendChild(todo__fulltitle);
+  todo__details.appendChild(todo__description);
+  todo__container.appendChild(todo__details);
+  return todo__container;
 }
 
 function showDetails(e) {
